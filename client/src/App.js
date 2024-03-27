e2b762fa1dcb210a54651ba3c8902f2fc4ada357
joannechen1223 <joannechen1223@gmail.com>
2024-03-27 02:53:58 -0400

    feat: add second sensor feedback

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import useWebSocket, { ReadyState } from 'react-use-websocket';
 import './App.css';
 
 const WS_URL = 'ws://100.110.142.168:8000';
-const circleCss = {
-  borderRadius: '50%',
-  width: '100px',
-  height: '100px'
-}
+const COLOR = {
+  RED: '#ee6055',
+  BLUE: '#48cae4',
+  WHITE: '#ffffff',
+};
 
 const App = () => {
-  const [color, setColor] = useState('white');
+  const [leftColor, setLeftColor] = useState(COLOR.WHITE);
+  const [rightColor, setRightColor] = useState(COLOR.WHITE);
   const { lastJsonMessage, readyState } = useWebSocket(WS_URL, {
     onOpen: () => {
       console.log('WebSocket connection established.');
@@ -18,28 +19,33 @@ const App = () => {
     share: true,
     filter: () => true,
     retryOnError: true,
-    shouldReconnect: () => true
+    shouldReconnect: () => true,
   });
 
   useEffect(() => {
     if (lastJsonMessage && readyState === ReadyState.OPEN) {
-      setColor('red');
-      setTimeout(() => setColor('white'), 200);
+      if (lastJsonMessage.sensor === 1) {
+        setLeftColor(COLOR.RED);
+        setTimeout(() => setLeftColor(COLOR.WHITE), 200);
+      }
+      if (lastJsonMessage.sensor === 2) {
+        setRightColor(COLOR.BLUE);
+        setTimeout(() => setRightColor(COLOR.WHITE), 200);
+      }
     }
   }, [lastJsonMessage, readyState]);
-
-
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          cajon-doni
-        </p>
-        <div style={{ backgroundColor: color, ...circleCss }}></div>
+        <p>cajon-doni</p>
+        <div className="Container">
+          <div className="Drum" style={{ backgroundColor: leftColor }}></div>
+          <div className="Drum" style={{ backgroundColor: rightColor }}></div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
